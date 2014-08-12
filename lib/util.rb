e8400e243550dd7354c5b5cd516ba993e0d27f53
Julian Browne <julianbrowne@gmail.com>
2014-08-12 09:05:09 +0100

    json keys/values extractors

--- lib/util.rb
+++ lib/util.rb
@@ -18,6 +18,137 @@ class Util
         return docs
     end
 
+    def self.generate_name(key, parent)
+        #puts "parent: #{parent.inspect} key: #{key.inspect}"
+        if parent
+            levels = parent.split('.')
+            if levels.last.to_i.to_s == levels.last
+                key_tag = levels[0..-2].join('.')
+                "#{key_tag}[n].#{key}"
+            else
+                levels = key.to_s.split('.')
+                if levels.last.to_i.to_s == levels.last
+                    key_tag = levels[0..-2].join('.')
+                    key_tag = ".#{key_tag}" if key_tag != ""
+                    "#{parent}#{key_tag}[n]"
+                else
+                    "#{parent}.#{key}"
+                end
+            end
+        else
+            levels = key.to_s.split('.')
+            if levels.last.to_i.to_s == levels.last
+                key_tag = levels[0..-2].join('.')
+                "#{key_tag}[n]"
+            else
+                "#{key}"
+            end
+        end
+    end
+
+    def self.json_keys(obj, parent=nil)
+        return [] if obj == nil
+        list = []
+        obj.each_with_index.find_all do |key_value, index|
+            key,value = key_value
+            if !value
+                name = generate_name(index, parent)
+                leader = parent ? "#{parent}.#{index}" : "#{index}"
+                case key
+                    when Array
+                        list << "#{name}[]"
+                        list << json_keys(key,"#{leader}")
+                    when Hash
+                        list << "#{name}"
+                        list << json_keys(key,"#{leader}")
+                    else
+                        name = "#{parent}[n]"
+                        name = "#{parent}#{parent ? '.' : ''}#{key}" if obj[index].inspect == 'nil'
+                        list << name
+                end
+            else
+                name = generate_name(key, parent)
+                leader = parent ? "#{parent}.#{key}" : "#{key}"
+                case value
+                    when Array
+                        list << "#{name}[]"
+                        list << json_keys(value,"#{leader}")
+                    when Hash
+                        list << "#{name}"
+                        list << json_keys(value,"#{leader}")
+                    else
+                        list << "#{name}"
+                end
+            end
+        end
+        return list.flatten.uniq
+    end
+
+    def self.json_values(obj, parent=nil)
+        return [] if obj == nil
+        list = []
+        obj.each_with_index.find_all do |key_value, index|
+            key,value = key_value
+            if !value
+                name = generate_name(index, parent)
+                leader = parent ? "#{parent}.#{index}" : "#{index}"
+                case key
+                    when Array
+                        list << json_values(key,"#{leader}")
+                    when Hash
+                        list << json_values(key,"#{leader}")
+                    else
+                        name = "#{parent}[n]"
+                        name = "#{parent}#{parent ? '.' : ''}#{key}" if obj[index].inspect == 'nil'
+                        v2 = key
+                        v2 = nil if obj[index].inspect == 'nil'
+                        list << { "#{name}" => v2 }
+                end
+            else
+                name = generate_name(key, parent)
+                leader = parent ? "#{parent}.#{key}" : "#{key}"
+                case value
+                    when Array
+                        list << json_values(value,"#{leader}")
+                    when Hash
+                        list << json_values(value,"#{leader}")
+                    else
+                        list << { "#{name}" => value }
+                end
+            end
+        end
+        return list.flatten.uniq
+    end
+
+=begin
+    def self.json_values(obj, parent=nil)
+        list = []
+        obj.each_with_index.find_all do |kv,i|
+            k,v=kv
+            if !v
+                v=k; k=i
+            end
+            leader = parent ? "#{parent}.#{k}" : "#{k}"
+            if v.class==Hash||v.class==Array
+                list << json_values(v,leader)
+            else
+                if parent
+                    levels = parent.split('.')
+                    if levels.last.to_i.to_s == levels.last
+                        # array index so snip off end
+                        list << {"#{levels[0..-2].join}.#{k}" => v}
+                    else
+                        list << {"#{leader}" => v}
+                    end
+                else
+                    list << {"#{leader}" => v}
+                end
+            end
+        end
+        return list.flatten
+    end
+=end
+
     def self.usage
         "usage: schemer {json_source_directory}"
     end
